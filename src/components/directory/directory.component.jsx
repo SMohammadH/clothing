83af018176358ee0cf8e9299ae06db509f073a92
Mohammad Hashemi <mohammadha1372@gmail.com>
2021-01-19 05:57:11 +0330

    added header

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,8 +48,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ key, ...otherSectionProps }) => (
-          <MenuItem key={key} {...otherSectionProps} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
